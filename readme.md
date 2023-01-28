5cd30ea6f5a0d02fc0db95b6e6cbe019fef9ffdb
Dan Lauerman <dan@fullstackagents.com>
2023-01-27 23:55:08 -0600

    Demo Finished

---
+++ readme.md
@@ -0,0 +1,32 @@
+### Azure Key Vault App Config Demo
+
+This demo shows how to use Azure Keyvault secrets to set application configuration settings
+
+
+### Following the Demo
+
+1. Build the docker image locally to run
+2. Push the docker image out to a docker hub
+3. Update main.tf file kv-name var with a unique keyvault name
+3. Update main.tf file with the docker image name
+4. Update main.tf file with the web app name (needs to be unique)
+5. Run az login at the terminal to authenticate to Azure
+6. terraform init in the ./terraform directory
+7. terraform apply in the ./terraform directory
+8. Go and add a secret to the keyvault that was created (through Azure portal)
+9. Update the application configuration key name in the app service to CONFIG_VALUE_SAMPLE
+10. Update the application configuration value in the app service to the name of your secret
+11. Call the endpoint {your website}/config_value to see the value
+12. Change the secret value
+13. run commands below to add fake app settings
+14. Call the endpoint {your website}/config_value to see the new value
+
+
+# Forcing a fake app setting to reload Keyvault based configurations
+
+echo "current settings---"
+az webapp config appsettings list --name ase-app-config-test --resource-group rg-web-app
+echo "create temp setting---"
+az webapp config appsettings set  --name ase-app-config-test --resource-group rg-web-app --settings my_temp_setting=ThisMakesTotalSense
+echo "delete temp setting---"
+az webapp config appsettings delete  --name ase-app-config-test --resource-group rg-web-app --setting-names my_temp_setting 
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,48 @@
 provider "azurerm" {
   features {}
 }
+variable "kv-name" {
+  description = "Set name of keyvault"
+  default = "INSERT-KEYVAULT-NAME"
+}
+
+data "azurerm_client_config" "current" {}
 
 resource "azurerm_resource_group" "rg-web-app-configs" {
   name     = "rg-web-app"
   location = "Central US"
+}
+
+
+resource "azurerm_key_vault" "keyvault" {
+  name                        = var.kv-name
+  location                    = azurerm_resource_group.rg-web-app-configs.location
+  resource_group_name         = azurerm_resource_group.rg-web-app-configs.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+
+  sku_name = "standard"
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    secret_permissions = [
+      "Get", "Delete", "Set", "List"
+    ]
+
+  }
+
+    access_policy {
+    tenant_id = azurerm_linux_web_app.web-app.identity[0].tenant_id
+    object_id = azurerm_linux_web_app.web-app.identity[0].principal_id
+
+    secret_permissions = [
+      "Get", "List"
+    ]
+  }
 }
 
 resource "azurerm_service_plan" "free-linux" {
@@ -15,8 +53,8 @@ resource "azurerm_service_plan" "free-linux" {
   sku_name            = "B1"
 }
 
-resource "azurerm_linux_web_app" "example" {
-  name                = "ase-app-config-test"
+resource "azurerm_linux_web_app" "web-app" {
+  name                = "asfgadhgg"
   resource_group_name = azurerm_resource_group.rg-web-app-configs.name
   location            = azurerm_service_plan.free-linux.location
   service_plan_id     = azurerm_service_plan.free-linux.id
@@ -25,13 +63,14 @@ resource "azurerm_linux_web_app" "example" {
     container_registry_use_managed_identity = false
 
     application_stack {
-      docker_image     = "dlaue/azure-container-web-app"
+      docker_image     = "dlau/asdfgag"
       docker_image_tag = "latest"
     }
   }
 
-  app_settings                      = {
+  app_settings = {
     DOCKER_REGISTRY_SERVER_URL = "https://index.docker.io/v1"
+    "CONFIG_VALUE_SAMPLE_KV"   = "@Microsoft.KeyVault(SecretUri=https://${var.kv-name}.vault.azure.net/secrets/ADD-SECRET-HERE/)"
   }
 
   identity {
@@ -39,3 +78,4 @@ resource "azurerm_linux_web_app" "example" {
   }
 
 }
+
